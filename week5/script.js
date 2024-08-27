c6d556d7e5ac662602ab9c7d664938e20ee1387a
s4001376 <s4001376@student.rmit.edu.au>
2024-08-27 11:01:32 +1000

    Created and Learning JavaScript

---
+++ week5/script.js
@@ -0,0 +1,34 @@
+console.log("My name is Markiplier");
+// this is a comment 
+let myName = "Liam Gibbons";
+// let name = prompt("What is your name?");
+const myCity = "Melbourne";
+console.log(myName, "lives in", myCity);
+
+console.log(`${myName}
+    
+    lives in
+   
+    ${myCity}`);
+
+let count = 10;
+console.log(count);
+
+let isItMorningClass = true;
+let isItAfternoonClass = false;
+
+let myStudentRecord = {
+    name: "Didrika",
+    id: "s4001376",
+    class: "0AT1013",
+    isItScience: false,
+    isItDesign: true,
+}
+
+console.log("Hello everyonem my name is", myStudentRecord.name);
+console.log("my id is", myStudentRecord.id);
+console.log("my class is", myStudentRecord.class);
+console.log("am I a science student?", myStudentRecord.isItScience);
+
+const numberArray = [2, 4, 6, 8, 10];
+console.log(numberArray [2]);
