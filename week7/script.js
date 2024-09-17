16b97b31d1bbb3372fe243c18d6f0da342cf0b71
s4001376 <s4001376@student.rmit.edu.au>
2024-09-17 11:40:23 +1000

    Week7 activity

---
+++ week7/script.js
@@ -0,0 +1,35 @@
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+const popSound = document.querySelector("#pop-sound");
+console.log(popSound);
+
+popButton.addEventListener("click", playPopSound);
+
+function playPopSound()
+{
+    popSound.play();
+}
+
+
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+const notify = document.querySelector("#notify");
+console.log(notify);
+
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+playButton.addEventListener("click", playVideo);
+function playVideo()
+{
+    myVideo.play();
+}
+
+pauseButton.addEventListener("click", pauseVideo);
+function pauseVideo()
+{
+    myVideo.pause();
+}
